789c46241e77490b71a51b0668f3781a2813ca6f
Amol Kadam <mygeat07@gmail.com>
2022-09-15 00:34:02 +0530

    Variables

---
+++ variables.js
@@ -0,0 +1,29 @@
+var n1 = 5;
+var n2 = 5;
+var sum = n1+n2;
+console.log(sum);
+
+//DATATYPES
+
+var str1= "stark";
+var no = 1.1;
+
+
+//object datatype
+var car = {
+ 
+    gavi : 7,
+    pedri:9,
+    busquets : 12,
+    dejong :10
+}
+
+//boolean datatype
+
+var b1 = true;
+var b2 = false;
+
+//array
+var a1 = [1,2,3,4, "Olaf",5];
+console.log(a1);
+console.log(a1[3]);
