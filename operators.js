3a76ac256660626469b177c5e63083a83e70ccb8
Amol Kadam <mygeat07@gmail.com>
2022-09-18 23:24:19 +0530

    basic operators

---
+++ operators.js
@@ -0,0 +1,21 @@
+var a = 10;
+var b = 10;
+
+
+// Arithmatic Operators
+console.log("a+b:",  a+b);
+console.log("a-b:",  a-b);
+console.log("a*b:",  a*b);
+console.log("a/b:",  a/b);
+
+//Comparison Operators
+var x=10;
+var y=15;
+console.log(x>y);
+
+// Logical Operators
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+console.log(true ||true);
+
