38aaa0a5514ed78762e15e5c8dc4591d12004714
Amol Kadam <mygeat07@gmail.com>
2022-09-25 13:14:55 +0530

    largest among 3 numbers

---
+++ if_else.js
@@ -0,0 +1,29 @@
+var n1 = 54;
+var n2 = 97;
+var n3 = 111;
+
+if(n1>n2)
+{
+    if(n1>n3)
+    {
+        console.log(n1 , "Is Largest");
+    }
+    else
+    {
+        console.log(n3 , "Is Largest");
+
+    }
+}
+else
+{
+    if(n2>n3)
+    {
+        console.log(n2 , "Is Largest");
+    }
+    else
+    {
+        console.log(n3 , "Is Largest");
+
+    }
+}
+
