f05b736467b14f39969cb092df326b670fdaccc1
Amol Kadam <mygeat07@gmail.com>
2022-09-26 22:20:09 +0530

    break and continue statement

---
+++ break_continue.js
@@ -0,0 +1,24 @@
+var a1 = [1,2,3,4,5];
+
+    console.log("Break:");
+
+for(i=0; i<a1.length; i++)
+{
+    if(i==2){
+     break;
+    }
+    console.log(a1[i]);
+}
+
+
+
+    console.log("Continue:");
+
+for(i=0; i<a1.length; i++)
+{
+    if(i==2){
+     continue;
+    }
+    console.log(a1[i]);
+}
+
