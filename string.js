970f51ca8507345d9dbcedebce2b6dd2856bd146
Amol Kadam <mygeat07@gmail.com>
2022-09-30 23:36:54 +0530

    string and its methods

---
+++ string.js
@@ -0,0 +1,8 @@
+let s1 = "Du wirst niemals alleine gehen";
+
+//console.log(s1.length);
+//console.log(s1.indexOf("niemals"))
+//console.log(s1.slice(4,5));
+
+og=s1.replace("wirst","hast");
+console.log(og);
