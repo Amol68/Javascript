3b06c28c1bd7f68768b887d125d447b432c22fef
Amol Kadam <mygeat07@gmail.com>
2022-10-03 22:57:50 +0530

    Strings

--- string.js
+++ string.js
@@ -4,5 +4,10 @@ let s1 = "Du wirst niemals alleine gehen";
 //console.log(s1.indexOf("niemals"))
 //console.log(s1.slice(4,5));
 
-og=s1.replace("wirst","hast");
-console.log(og);
+//og=s1.replace("wirst","hast");
+//console.log(og);
+
+let d = new Date();
+console.log(d);
+console.log(d.getTime());
+console.log(d.getFullYear())
